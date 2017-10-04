e399fe0901b239a5a7ab196af80ca2df0eabfafe
CrookedWay <wjackso1@unca.edu>
2017-10-04 19:23:20 -0400

    Input functionality added.

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
+import asyncio
+import argparse
+
 from Board import *
 from Move import *
 
+trace = False
+
+gameOver = False
+
+alphaNumeric = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8}
+
+myTurn = False
+myColor = None
+opponentColor = None
+
+
 startBoard = Board("startBoard")
 startBoard.initalizeBoard()
-startBoard.printBoard()
-nextMove = Move("NextMove")
-nextMove.moveList(startBoard, '1')
+
+#Game Begins.
+colorChoice = input()
+if colorChoice == "I W":
+    myColor = "w"
+    opponentColor = "b"
+    print("R W")
+if colorChoice == "I B":
+    myColor = "b"
+    opponentColor = "w"
+    print("R B")
+
+while gameOver != True:
+    if myTurn == False:
+        currentAction = input()
+        if len(currentAction) == 1 and currentAction[0] != "n":
+                print("Pass")
+        elif currentAction[0] == "n":
+            gameOver = True
+        else:
+            column = int(alphaNumeric[currentAction[2]])
+            row = int(currentAction[4])
+            startBoard.takeMove(column, row, opponentColor)
+        if trace == True:
+            startBoard.prettyPrintBoard()
+    if myTurn == True:
+# nextMove = Move("NextMove")
+# nextMove.moveList(startBoard, '1')
+
+
+
