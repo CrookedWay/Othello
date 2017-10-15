89525c3864e839b572215267b5554b7dac3fc420
CrookedWay <wjackso1@unca.edu>
2017-10-14 23:16:25 -0400

    Pretty much changed move entirely. I still have occasional illegal moves.

--- Move.py
+++ Move.py
@@ -11,101 +11,44 @@ class Move:
         self.color = color
         self.enemyColor = enemyColor
 
-
     def moveList(self, gameBoard):
-        for (x, y), element in np.ndenumerate(gameBoard.gameBoard):
-            if element == 0:
-                # Check South
-                for check_x in range(x, 8):
-                    if gameBoard.gameBoard[y, check_x] == 0 and check_x != x:
-                        break
-                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
-                        continue
-                    if gameBoard.gameBoard[y, check_x] == self.color:
-                        self.possibleMoves.append([y, x, "South"])
-                        break
-                # Check North
-                for check_x in range(x, 0, -1):
-                    if gameBoard.gameBoard[y, check_x] == 0 and check_x != x:
-                        break
-                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
-                        continue
-                    if gameBoard.gameBoard[y, check_x] == self.color:
-                        self.possibleMoves.append([y, x, "North"])
-                        break
-                # Check East
-                for check_y in range(y, 8):
-                    if gameBoard.gameBoard[check_y, x] == 0 and check_y != y:
-                        break
-                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
-                        continue
-                    if gameBoard.gameBoard[y, check_x] == self.color:
-                        self.possibleMoves.append([y, x, "East"])
-                        break
-                # Check West
-                for check_y in range(y, 0, -1):
-                    if gameBoard.gameBoard[check_y, x] == 0 and check_y != y:
-                        break
-                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
-                        continue
-                    if gameBoard.gameBoard[y, check_x] == self.color:
-                        self.possibleMoves.append([check_y, x, "East"])
-                # Check NE
-                for check_x, check_y in zip(range(x, 0, -1), range(y, 8)):
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and check_y != y:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
-                        continue
-                    if gameBoard.gameBoard[check_y, check_x] == self.color:
-                        self.possibleMoves.append([check_y, x, "East"])
-                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y - 1, check_x + 1] == 0:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y - 1, check_x + 1] == self.enemyColor:
-                        self.possibleMoves.append([check_y, check_x, "NE"])
-                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
-                        continue
-                # Check NW
-                for check_x, check_y in zip(range(x, 0, -1), range(y, 0, -1)):
-                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x + 1] == 0:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x + 1] == self.enemyColor:
-                        self.possibleMoves.append([check_y, check_x, "NW"])
-                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
-                        continue
-                # Check SE
-                for check_x, check_y in zip(range(x, 8), range(x, 0, -1)):
-                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x - 1] == 0:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x - 1] == self.enemyColor:
-                        self.possibleMoves.append([check_y, check_x, "SE"])
-                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
-                        continue
-                # Check SE
-                for check_x, check_y in zip(range(x, 8), range(x, 0, -1)):
-                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x - 1] == 0:
-                        break
-                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
-                                check_y + 1, check_x - 1] == self.enemyColor:
-                        self.possibleMoves.append([check_y, check_x, "SE"])
-                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
-                        continue
+        exit = False
+        zeroIndex = 0
+        for (initial_y, initial_x), element in np.ndenumerate(gameBoard.gameBoard):
+            if element == self.color:
+                x = initial_x
+                y = initial_y
+                for directional_y, directional_x in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0],
+                                                     [-1, 1]]:
+                    while exit != True:
+                        x += directional_x
+                        y += directional_y
+                        if self.isValid(y, x) == False:
+                            x = initial_x
+                            y = initial_y
+                            break
+                        if gameBoard.gameBoard[y][x] == self.enemyColor:
+                            continue
+                        if gameBoard.gameBoard[y][x] == 0 and gameBoard.gameBoard[y - directional_y][
+                                    x - directional_x] == 0:
+                            x = initial_x
+                            y = initial_y
+                            break
+                        if gameBoard.gameBoard[y][x] == 0 and gameBoard.gameBoard[y - directional_y][
+                                    x - directional_x] == self.enemyColor:
+                            self.possibleMoves.append([x, y, directional_x, directional_y])
         return self.possibleMoves
 
+    def hook(self, gameBoard):
+        for moves in self.moveList():
+            isIsland = True
+            x, y = moves[0], moves[1]
+            for directional_y, directional_x in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0],
+                                                 [-1, 1]]:
+                if gameBoard.gameBoard[y - directional_y][x - directional_x] != 0:
+                    isIsland = False
+                    del self.moveList[moves]
+                    break
 
     def printMoves(self):
         for x in self.possibleMoves:
@@ -120,91 +63,6 @@ class Move:
     def emptyMoves(self):
         del self.possibleMoves[:]
 
-
- # for check_x in range(x, 8):
- #                    if gameBoard.gameBoard[y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[y, check_x] == 0 and gameBoard.gameBoard[y, check_x - 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[y, check_x] == 0 and gameBoard.gameBoard[y, check_x - 1] == self.enemyColor:
- #                        self.possibleMoves.append([y, check_x, "South"])
- #                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
- #                        continue
- #                # Check North
- #                for check_x in range(x, 0, -1):
- #                    if gameBoard.gameBoard[y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[y, check_x] == 0 and gameBoard.gameBoard[y, check_x + 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[y, check_x] == 0 and gameBoard.gameBoard[y, check_x + 1] == self.enemyColor:
- #                        self.possibleMoves.append([y, check_x, "North"])
- #                    if gameBoard.gameBoard[y, check_x] == self.enemyColor:
- #                        continue
- #                # Check East
- #                for check_y in range(y, 8):
- #                    if gameBoard.gameBoard[check_y, x] == self.color and check_y != y:
- #                        break
- #                    if gameBoard.gameBoard[check_y, x] == 0 and gameBoard.gameBoard[check_y - 1, x] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, x] == 0 and gameBoard.gameBoard[check_y - 1, x] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, x, "East"])
- #                    if gameBoard.gameBoard[check_y, x] == self.enemyColor:
- #                        continue
- #                # Check West
- #                for check_y in range(y, 0, -1):
- #                    if gameBoard.gameBoard[check_y, x] == self.color and check_y != y:
- #                        break
- #                    if gameBoard.gameBoard[check_y, x] == 0 and gameBoard.gameBoard[check_y + 1, x] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, x] == 0 and gameBoard.gameBoard[check_y + 1, x] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, x, "West"])
- #                    if gameBoard.gameBoard[check_y, x] == self.enemyColor:
- #                        continue
- #                # Check NE
- #                for check_x, check_y in zip(range(x, 0, -1), range(y, 8)):
- #                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y - 1, check_x + 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y - 1, check_x + 1] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, check_x, "NE"])
- #                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
- #                        continue
- #                # Check NW
- #                for check_x, check_y in zip(range(x, 0, -1), range(y, 0, -1)):
- #                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x + 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x + 1] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, check_x, "NW"])
- #                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
- #                        continue
- #                # Check SE
- #                for check_x, check_y in zip(range(x, 8), range(x, 0, -1)):
- #                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x - 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x - 1] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, check_x, "SE"])
- #                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
- #                        continue
- #                # Check SE
- #                for check_x, check_y in zip(range(x, 8), range(x, 0, -1)):
- #                    if gameBoard.gameBoard[check_y, check_x] == self.color and check_x != x:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x - 1] == 0:
- #                        break
- #                    if gameBoard.gameBoard[check_y, check_x] == 0 and gameBoard.gameBoard[
- #                                check_y + 1, check_x - 1] == self.enemyColor:
- #                        self.possibleMoves.append([check_y, check_x, "SE"])
- #                    if gameBoard.gameBoard[check_y, check_x] == self.enemyColor:
- #                        continue
+    def isValid(self, x, y):
+        if x > 7 or x < 0 or y > 7 or y < 0:
+            return False
